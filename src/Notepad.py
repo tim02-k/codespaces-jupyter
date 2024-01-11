06ff818640853864a8d5ce4dd4befa286ad57464
Tim Kerl <tim.kerl.02@gmx.de>
2024-01-11 17:59:01 +0100

    Finished Notepad

--- src/Notepad.py
+++ src/Notepad.py
@@ -53,3 +53,45 @@ class Notepad:
         self.__thisScrollBar.pack(side=RIGHT, fill=Y)
         self.__thisScrollBar.config(command=self.__thisTextArea.yview)
         self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
+    def __quitApplication(self):
+        self.__root.destroy()
+    def __showAbout(self):
+        showinfo("Notepad", "Mrinal Verma")
+    def __openFile(self):
+        self.__file = askopenfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+        if self.__file == "":
+            self.__file = None
+        else:
+            self.__root.title(os.path.basename(self.__file) + " - Notepad")
+            self.__thisTextArea.delete(1.0, END)
+            file = open(self.__file, "r")
+            self.__thisTextArea.insert(1.0, file.read())
+            file.close()
+    def __newFile(self):
+        self.__root.title("Untitled - Notepad")
+        self.__file = None
+        self.__thisTextArea.delete(1.0, END)
+    def __saveFile(self):
+        if self.__file == None:
+            self.__file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+            if self.__file == "":
+                self.__file = None
+            else:
+                file = open(self.__file, "w")
+                file.write(self.__thisTextArea.get(1.0, END))
+                file.close()
+                self.__root.title(os.path.basename(self.__file) + " - Notepad")
+        else:
+            file = open(self.__file, "w")
+            file.write(self.__thisTextArea.get(1.0, END))
+            file.close()
+    def __cut(self):
+        self.__thisTextArea.event_generate("<<Cut>>")
+    def __copy(self):
+        self.__thisTextArea.event_generate("<<Copy>>")
+    def __paste(self):
+        self.__thisTextArea.event_generate("<<Paste>>")
+    def run(self):
+        self.__root.mainloop()
+notepad = Notepad(width=600, height=400)
+notepad.run()

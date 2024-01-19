38e03a1aa07cd046e4dcd824963e2678971514d5
Tim Kerl <tim.kerl.02@gmx.de>
2024-01-19 13:13:38 +0100

    Print the snake body, try to implement the steering

--- src/snake.py
+++ src/snake.py
@@ -11,7 +11,35 @@ c.pack()
 grid_length= 23
 
 body_img = Image.open("snake_body.png")
-body_img = body_img.resize((grid_length, grid_length), Image.ANTIALIAS)
+body_img = body_img.resize((grid_length, grid_length))
 body_img = ImageTk.PhotoImage(body_img)
 richtung = (1,0)
 head_pos = [B/2, H/2]
+
+def move_head():
+    head_pos[0]+= richtung[0]*grid_length
+    head_pos[1]+= richtung[1]*grid_length
+
+def new_body():
+    c.create_image(head_pos[0], head_pos[1], image=body_img)
+
+sleep_seconds = 0.2
+while True:
+    move_head()
+    new_body()
+    window.update()
+    sleep(sleep_seconds)
+
+# Richtung ändern
+def richtung_aendern(event):
+    global richtung
+    if event.keysym == "Up":
+        richtung = (0,-1)
+    elif event.keysym == "Down":
+        richtung= (0,1)
+    elif event.keysym == "Left":
+        richtung = (-1,0)
+    elif event.keysym == "Right":
+        richtung = (1,0)
+
+c.bind_all("<Key>", richtung_aendern)
